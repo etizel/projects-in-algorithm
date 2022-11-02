472ca0688d947285486db368036b1ff37181d49b
etiiizel <franciscoetizel@gmail.com>
2022-11-02 17:14:02 -0300

    first commit

---
+++ horas trabalhadas/app.js
@@ -0,0 +1,12 @@
+
+var salarioMensal = 2000;
+var horaMensal = 200;
+
+function salarioTotal() {
+  let hora = salarioMensal / horaMensal;
+
+  console.log(`Tuas horas trabalhadas são de ${hora};`)
+
+} 
+
+salarioTotal();
