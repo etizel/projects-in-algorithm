d809663f7b2707838f03ed463022619037c3a73b
etiiizel <franciscoetizel@gmail.com>
2022-11-04 22:23:26 -0300

    taxa inclusiva II

--- taxa inclusiva/taxa.js
+++ taxa inclusiva/taxa.js
@@ -1,1 +1,10 @@
-console.log('oi')
+
+function Carros(mark, price) {
+  const marca = mark;
+  const preco = price;
+  const taxa = 3;
+  const precoFinal = preco * taxa;
+  
+  console.log(marca, preco, precoFinal)
+ 
+};
