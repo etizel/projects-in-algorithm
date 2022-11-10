fc77521a1694435d7530216e61b78e0a5ece1637
etiiizel <franciscoetizel@gmail.com>
2022-11-09 21:49:43 -0300

    prototype IV

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -15,3 +15,14 @@ const etizel = new Pessoas('Etizel','Azevedo', 27);
 const ana = new Pessoas('Ana','Maria', 23);
 const fabio = new Pessoas('Fabio','Pamplona', 45);
 const luiz = new Pessoas('Luiz','Malfito', 27);
+
+
+
+// prototype Array
+
+
+const nodeList = document.querySelectorAll('li');
+console.log(nodeList);
+
+const listArray = Array.prototype.slice.call(nodeList);
+const listArray2 = Array.from(nodeList);
