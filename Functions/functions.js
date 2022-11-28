b32fbba67ef733333d8ae5a7fde66faf86969c21
etiiizel <franciscoetizel@gmail.com>
2022-11-28 20:03:21 -0300

    Functions

---
+++ Functions/functions.js
@@ -0,0 +1,18 @@
+
+
+// função construtora para criação de vários objetos dentro de um método universal
+function champs() {
+  return `${this.power} ${this.money}`
+}
+
+
+const lissandra = {
+  power: 'Tûmulo congelado',
+  money: 7900,
+};
+
+
+const nami = {
+  power: 'Maré Violenta',
+  money: 6500,
+};
