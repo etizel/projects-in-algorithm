1fdfbabddb2b01a06a5d9580eaf71d597875c54a
etiiizel <franciscoetizel@gmail.com>
2022-11-29 21:14:57 -0300

    genius

--- genius/genius.js
+++ genius/genius.js
@@ -2,3 +2,5 @@
 
 function genius() {
   var numberRandom = [Math.floor(Math.random(99) * (1 - 99 + 1)) + 1];
+  let user = prompt('Type one number');
+  if(Number(numberRandom) === Number(user.length)) {
