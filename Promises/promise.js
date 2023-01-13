a0d0bfce9ea3c1693917fd8966694d5251b6e97d
etiiizel <franciscoetizel@gmail.com>
2023-01-13 17:28:00 -0300

    finally Promises

---
+++ Promises/promise.js
@@ -0,0 +1,24 @@
+const promessa = new Promise((resolve, reject) => {
+  const condition = true;
+  if (condition) {
+    setTimeout(() => {
+      resolve({ name: 'Francisco Etizel', age: 27, approved: true });
+    }, 1000);
+  } else {
+    console.log(reject(Error('Um erro ocorreu na promise')));
+  }
+});
+
+const thenX = promessa
+  .then((resolve) => {
+    console.log(resolve);
+    return resolve;
+  })
+  .then((resolve1) => {
+    console.log(resolve1);
+  })
+  .finally((end) => {
+    const X = { name: 'kill bill', year: 2000 };
+    end = X;
+    console.log(end);
+  });
