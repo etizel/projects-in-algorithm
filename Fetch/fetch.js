54e39da4d0281138494ca5922241847fc6616115
etiiizel <franciscoetizel@gmail.com>
2023-01-14 18:49:53 -0300

    fetch()

--- Fetch/fetch.js
+++ Fetch/fetch.js
@@ -1,1 +1,14 @@
 // type basic
+const doc = fetch('./style.css');
+
+doc
+  .then((r) => r.json())
+  .then((body) => {
+    const conteudo = document.querySelector('.conteudo');
+    const style = document.createElement('style');
+    style.innerHTML = body;
+    console.log(style);
+    conteudo.appendChild(style);
+  });
+
+// type cors
