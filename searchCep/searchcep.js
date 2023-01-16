88c534a52296e160e7f5a107ca1fc3a935bc565b
etiiizel <franciscoetizel@gmail.com>
2023-01-15 21:11:31 -0300

    buscador de cep

---
+++ searchCep/searchcep.js
@@ -0,0 +1,20 @@
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('btncep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep);
+  console.log(event);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((response) => response.text())
+    .then((body) => {
+      resultadoCep.innerText = body;
+    });
+}
