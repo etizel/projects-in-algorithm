b592e1225385e986f4e64cb825c3317f99305b9f
etiiizel <franciscoetizel@gmail.com>
2023-01-17 11:41:18 -0300

    joke for norris

--- joke pusher/joke.js
+++ joke pusher/joke.js
@@ -1,1 +1,14 @@
+const chuky = document.querySelector('.chuky');
+const nextX = document.querySelector('.next');
 
+function pushJoke() {
+  const fetchNorris = fetch('https://api.chucknorris.io/jokes/random');
+
+  fetchNorris
+    .then((response) => response.json())
+    .then((jsonNorris) => {
+      chuky.innerText = jsonNorris.value;
+    });
+}
+
+nextX.addEventListener('click', pushJoke);
