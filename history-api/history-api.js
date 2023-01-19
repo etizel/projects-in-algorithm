12f1eb35bbeb9b4a5dd8cf37c0772f5b9808942c
etiiizel <franciscoetizel@gmail.com>
2023-01-19 14:57:18 -0300

    history-api.js

---
+++ history-api/history-api.js
@@ -0,0 +1,32 @@
+const links = document.querySelectorAll('a');
+
+function handlePopState(event) {
+  event.preventDefault();
+  fetchUrl(event.target.href);
+  window.history.pushState(null, null, event.target.href);
+}
+
+async function fetchUrl(url) {
+  const selectFetch = await fetch(url);
+  const selectText = await selectFetch.text();
+  activeText(selectText);
+}
+
+function activeText(newText) {
+  const newHtml = document.createElement('div');
+  newHtml.innerHTML = newText;
+
+  const oldContent = document.querySelector('.content');
+  const newContent = newHtml.querySelector('.content');
+
+  oldContent.innerHTML = newContent.innerHTML;
+  document.title = newHtml.querySelector('title').innerText;
+}
+
+window.addEventListener('popstate', () => {
+  fetchUrl(window.location.href);
+});
+
+links.forEach((item) => {
+  item.addEventListener('click', handlePopState);
+});
