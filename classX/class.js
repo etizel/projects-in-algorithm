fe11471fc3760f398947b7801587f8246ff731de
etiiizel <franciscoetizel@gmail.com>
2023-01-22 12:57:16 -0300

    class

--- classX/class.js
+++ classX/class.js
@@ -12,3 +12,18 @@ class Button {
 
 const buttonPurple = new Button('Sell this', 'purple');
 const buttonPink = new Button('Sell this', 'pink');
+
+class Button {
+  constructor(options) {
+    this.options = options;
+  }
+  static createButton(text, background) {
+    const buttonElement = document.createElement('button');
+    buttonElement.innerText = text;
+    buttonElement.style.background = background;
+    return buttonElement;
+  }
+}
+
+const buttonStatik = Button.createButton('this is a button 2', 'lightpink');
+console.log(buttonStatik);
