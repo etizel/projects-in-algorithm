c459f45cc3ca7774f2332c21b35430e1c7cb5a9c
etiiizel <franciscoetizel@gmail.com>
2023-01-25 09:37:55 -0300

    plugin countdown

--- countdown-plugin/countdown.js
+++ countdown-plugin/countdown.js
@@ -2,5 +2,13 @@ export default class CountDown {
   constructor(futureDate) {
     this.futureDate = futureDate;
   }
-  get;
+  get _actualDate() {
+    return new Date();
+  }
+  get _futureDate() {
+    return new Date(this.futureDate);
+  }
+  get _timeStampDiff() {
+    return this.futureDate.getTime() - this._actualDate.getTime();
+  }
 }
