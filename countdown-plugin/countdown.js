483a962eab49e8358e9f6d45b84a70fc875e8d6d
etiiizel <franciscoetizel@gmail.com>
2023-01-25 09:35:19 -0300

    plugin countdown

---
+++ countdown-plugin/countdown.js
@@ -0,0 +1,6 @@
+export default class CountDown {
+  constructor(futureDate) {
+    this.futureDate = futureDate;
+  }
+  get;
+}
---
+++ countdown-plugin/script.js
@@ -0,0 +1,5 @@
+import CountDown from './countdown.js';
+
+const timeofChristimas = new CountDown('24 December 2023 23:59:59 GMT-0300');
+
+console.log(timeofChristimas);
