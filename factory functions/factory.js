d3dd6d1c5d06d13954c1314355a3aa07aaced076
etiiizel <franciscoetizel@gmail.com>
2023-01-26 19:42:44 -0300

    factory functions

---
+++ factory functions/factory.js
@@ -0,0 +1,48 @@
+// create champs
+function createChamp(name, power, hability) {
+  return {
+    name,
+    power,
+    hability,
+    ulimate: function () {
+      console.log(
+        `The name is: ${this.name} and power level is: ${this.power} and this hability is: ${this.hability}`,
+      );
+    },
+  };
+}
+
+const sakuraHaruno = createChamp(
+  'Sakura Haruno',
+  6499,
+  'Okashou: Impact Blossom Cherry',
+);
+const sasukeUchiha = createChamp(
+  'Sasuke Uchiha',
+  7499,
+  'Amateratsu: Eternals Dark Flames',
+);
+console.log(sasukeUchiha, sakuraHaruno);
+console.log(sasukeUchiha.ulimate());
+console.log(sakuraHaruno.ulimate());
+
+// create buttons
+
+function createButton(text) {
+  function element() {
+    const buttonElement = document.createElement('button');
+    buttonElement.innerText = text;
+    return buttonElement;
+  }
+
+  return {
+    text,
+    element,
+  };
+}
+const btnSell = createButton('Buy');
+const btnBuy = createButton('Sell');
+
+console.log(btnSell, btnBuy);
+console.log(btnSell.element());
+console.log(btnBuy.element());
