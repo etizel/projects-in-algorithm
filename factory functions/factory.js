118ad0359263cca53707599c2a470c0fb8b9b236
etiiizel <franciscoetizel@gmail.com>
2023-01-28 12:07:55 -0300

    factory function

--- factory functions/factory.js
+++ factory functions/factory.js
@@ -46,3 +46,60 @@ const btnBuy = createButton('Sell');
 console.log(btnSell, btnBuy);
 console.log(btnSell.element());
 console.log(btnBuy.element());
+
+function $$(selectedElements) {
+  const elements = document.querySelectorAll(selectedElements);
+
+  function hide() {
+    elements.forEach((element) => {
+      element.style.display = 'none';
+    });
+    return this;
+  }
+
+  function show() {
+    elements.forEach((element) => {
+      element.style.display = 'initial';
+    });
+    return this;
+  }
+
+  function on(onEvent, callback) {
+    elements.forEach((element) => {
+      element.addEventListener(onEvent, callback);
+    });
+    return this;
+  }
+
+  function addClass(className) {
+    elements.forEach((element) => {
+      element.classList.add(className);
+    });
+    return this;
+  }
+
+  function removeClass(className) {
+    elements.forEach((element) => {
+      element.classList.remove(className);
+    });
+    return this;
+  }
+
+  return {
+    elements,
+    hide,
+    show,
+    on,
+    addClass,
+    removeClass,
+  };
+}
+
+const btns = $$('button');
+
+function handleClick(event) {
+  console.log(event.target);
+}
+
+btns.on('click', handleClick);
+btns.addClass('active');
