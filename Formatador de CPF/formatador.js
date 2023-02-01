cb455fa34a736310d730a3024a3b1efbde7404cf
etiiizel <franciscoetizel@gmail.com>
2023-02-01 19:31:22 -0300

    regex constructor

--- Formatador de CPF/formatador.js
+++ Formatador de CPF/formatador.js
@@ -3,3 +3,11 @@ const cpfsList = document.querySelectorAll('.cpf li');
 const elementsInnerText = ([...elements]) => {
   return elements.map((element) => element.innerText);
 };
+
+const cleanCPF = (cpf) => {
+  return cpf.replace(/\D/g, '');
+};
+
+const construirCPF = (cpf) => {
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+};
